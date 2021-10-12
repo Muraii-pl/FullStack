c24c053d092ca94c126f06d345203e51ac2c03a0
Muraii-pl <mateusz.glowania@gmail.com>
2021-10-12 12:39:21 +0200

    display all post and create post

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const db = require('./models')
+const postRouter = require('./routes/Posts')
+
+const app = express()
+app.use(express.json())
+
+
+
+app.use("/posts", postRouter)
+
+db.sequelize.sync().then(()=>{
+    app.listen(3001,()=>{
+        console.log("Server running ")
+    })
+})
